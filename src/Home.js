da30a02e2fa35fa8c1d351631d4b74cf53d41040
venkatafanendra <fanendra.yadlapalli@gmail.com>
2022-04-08 12:28:32 +0530

    Static Interface completed

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Product from './Product';
 import './Home.css';
 
 function Home() {
@@ -6,19 +7,19 @@ function Home() {
     <div className='home'>
         <div className='home__container'>
             <img className='home__image' src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg' alt='' />
-        </div>
-        <div className='home__row'>
-            {/*Product*/}
-            {/*Product*/}
-        </div>
-        <div className='home__row'>
-            {/*Product*/}
-            {/*Product*/}
-            {/*Product*/}
-        </div>
-        <div className='home__row'>
-            {/*Product*/}
-        </div>
+            <div className='home__row'>
+                <Product/>
+                <Product/>
+            </div>
+            <div className='home__row'>
+                <Product/>
+                <Product/>
+                <Product/>
+            </div>
+            <div className='home__row'>
+                <Product/>
+            </div>
+        </div> 
     </div>
     
   )
